637d7ac2b4fba45315a294f3db6e2d5fffc6eb90
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-01-30 16:21:19 +0800

    fs: susfs: Replaced susfs_is_boot_completed_triggered with susfs_is_sdcard_android_data_decrypted
    1st scene: There will be still some processes spawned by init after /data is decrypted, these processes will still go through the sucompat checks which takes more CPU cycle.
    
    2nd scene: There can be some mounts mounted after /data is decrypted like yt rvx module, these mounts will leave a mnt_id gap or mnt_group_id gap if it is not a bind mount.
    
    So to mark no sucompat checks for all init spawned processes, and to capture the ksu mounts and leave no mnt_id/mnt_group_id gaps as much as possible, here we extend the timing and stop the check only on 5 seconds after /sdcard/Android/data is accessible. And normally 5 seconds should be enough.

--- fs/exec.c
+++ fs/exec.c
@@ -1740,7 +1740,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 		goto orig_flow;
 	}
 
-	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
+	if (unlikely(ksu_execveat_hook || !	if (unlikely(ksu_execveat_hook || !susfs_is_sdcard_android_data_decrypted)) {)) {
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
@@ -1904,7 +1904,7 @@ out_ret:
 
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_execveat_hook __read_mostly;
-extern bool susfs_is_boot_completed_triggered __read_mostly;
+extern bool susfs_is_sdcard_android_data_decrypted __read_mostly;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
